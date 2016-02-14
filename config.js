8056b9e48c763bcbca3ecba3d0010559f9bbbe86
Jamie Penney <jamie@jamiepenney.co.nz>
2016-02-15 08:44:45 +1300

    Add default config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+﻿var config = {
+  slackToken: process.env.SLACK_TOKEN || 'NOTAREALTOKEN',
+  slackRoom: process.env.SLACK_ROOM || '#testing',
+  raygunApiKey: process.env.RAYGUN_API_KEY || '',
+  pg: process.env.DATABASE_URL || 'postgres://lunchtime:Password1@127.0.0.1:5432/lunchtime'
+};
+
+module.exports = config;
