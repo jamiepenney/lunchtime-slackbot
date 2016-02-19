47e0789cf90432f43c561a22a48496023f49e235
Jamie Penney <jamie@jamiepenney.co.nz>
2016-02-19 19:13:51 +1300

    If no keyword is present, reply with cat facts

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var config = require('./config')
 var token = require('./conversations/token');
 var vote = require('./conversations/vote');
 var db = require('./db');
+var request = require('request');
 
 var slack = new SlackClient.WebClient(config.slackToken);
 
@@ -32,63 +33,6 @@ var Botkit = require('botkit');
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
   token: config.slackToken
-});
-
-//////////////////////
-/// Hello response ///
-//////////////////////
-
-controller.hears(["hello", "hi", "hey"],["direct_message","direct_mention", "mention"],function(bot,message) {
-  getUser(message.user, function(err, user){
-    if(err){
-      return;
-    }
-    bot.reply(message, 'Hello ' + (user.profile.real_name || user.name));
-  });
-});
-
-/////////////////////
-/// Help response ///
-/////////////////////
-
-controller.hears(["help"],["direct_message","direct_mention", "mention"],function(bot,message) {
-  getUser(message.user, function(err, user){
-    if(err){
-      return;
-    }
-    bot.startConversation(message, function(err, conversation){
-      conversation.say('Hello ' + (user.profile.real_name || user.name));
-      conversation.say('I understand the following commands:');
-      conversation.say('`hello`: Says hello to you');
-      conversation.say('`token`: Sends you a private message with your token for voting on the website, and lets you change it');
-      conversation.say('`vote`: Starts the voting process');
-    });
-  });
-});
-
-///////////////////////
-/// Hungry response ///
-///////////////////////
-
-var notTodayResponses = ['Sorry friend not much I can do about that today!', "I'm sorry Dave, I'm afraid I can't do that", "Not today peeps"]
-
-controller.hears(["hungry", "lunch"],["direct_message","direct_mention", "mention"],function(bot,message) {
-  getUser(message.user, function(err, user){
-    if(err){
-      return;
-    }
-    db.getCurrentRound(function(err, current_round){
-      if(err) {
-        return;
-      }
-      if(current_round && current_round.winning_choice_id) {
-        bot.reply(message, randomResponse(notTodayResponses));
-      } else {
-        bot.reply(message, 'If you want to vote for lunch, just say vote');
-      }
-    })
-
-  });
 });
 
 ///////////////////////
@@ -134,6 +78,88 @@ controller.hears(["vote"],["direct_message"],function(bot,message) {
       return;
     }
     vote.conversation(bot, user, message, true);
+  })
+});
+
+
+///////////////////////
+/// Hungry response ///
+///////////////////////
+
+var notTodayResponses = ['Sorry friend not much I can do about that today!', "I'm sorry Dave, I'm afraid I can't do that", "Not today peeps"]
+
+controller.hears(["hungry", "lunch"],["direct_message","direct_mention", "mention"],function(bot,message) {
+  getUser(message.user, function(err, user){
+    if(err){
+      return;
+    }
+    db.getCurrentRound(function(err, current_round){
+      if(err) {
+        return;
+      }
+      if(current_round && current_round.winning_choice_id) {
+        bot.reply(message, randomResponse(notTodayResponses));
+      } else {
+        bot.reply(message, 'If you want to vote for lunch, just say vote');
+      }
+    })
+
+  });
+});
+
+
+/////////////////////
+/// Help response ///
+/////////////////////
+
+controller.hears(["help"],["direct_message","direct_mention", "mention"],function(bot,message) {
+  getUser(message.user, function(err, user){
+    if(err){
+      return;
+    }
+    bot.startConversation(message, function(err, conversation){
+      conversation.say('Hello ' + (user.profile.real_name || user.name));
+      conversation.say('I understand the following commands:');
+      conversation.say('`hello`: Says hello to you');
+      conversation.say('`token`: Sends you a private message with your token for voting on the website, and lets you change it');
+      conversation.say('`vote`: Starts the voting process');
+    });
+  });
+});
+
+
+//////////////////////
+/// Hello response ///
+//////////////////////
+
+controller.hears(["hello", "hi", "hey"],["direct_message","direct_mention", "mention"],function(bot,message) {
+  getUser(message.user, function(err, user){
+    if(err){
+      return;
+    }
+    bot.reply(message, 'Hello ' + (user.profile.real_name || user.name));
+  });
+});
+
+controller.hears(['.*'], ["direct_message","direct_mention", "mention"], function(bot, message){
+  getUser(message.user, function(err, user){
+    if(err){
+      return;
+    }
+    request('http://catfacts-api.appspot.com/api/facts', function (err, response, body){
+      if(err || response.statusCode !== 200) {
+        console.log(err || response);
+        return;
+      }
+      var facts = JSON.parse(body);
+      if(facts && facts.success){
+        bot.startConversation(message, function(err, conversation){
+          conversation.say('Hello ' + (user.profile.real_name || user.name));
+          conversation.say('Thank you for subscribing to cat facts');
+          conversation.say(facts.facts[0]);
+        });
+      }
+    })
   })
 });
 
