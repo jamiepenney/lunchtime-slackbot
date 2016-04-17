976bd976089e0196f0b024f0042522b8d83c0708
Jamie Penney <jamiepenney@users.noreply.github.com>
2016-04-17 15:30:03 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 ﻿var config = {
-  slackToken: process.env.SLACK_TOKEN || 'xoxb-4593801382-vAtjVSzAGeOgXeyfXdHdr0eH',
+  slackToken: process.env.SLACK_TOKEN || 'EXAMPLE_TOKEN',
   raygunApiKey: process.env.RAYGUN_API_KEY || '',
   pg: process.env.DATABASE_URL || 'postgres://lunchtime:Password1@127.0.0.1:5432/lunchtime'
 };
